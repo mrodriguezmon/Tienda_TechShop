ef7993a9dd538fbf20c9c289b2950aa1c23e7fbb
fernandaurena <fernandaurena@localhost>
2024-06-09 13:37:24 -0400

    Inicializacion del proyecto

---
+++ src/main/java/com/engineeringhowto/EngineeringhowtoApplication.java
@@ -0,0 +1,13 @@
+package com.engineeringhowto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringhowtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringhowtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineeringhowto/EngineeringhowtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringhowto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringhowtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
