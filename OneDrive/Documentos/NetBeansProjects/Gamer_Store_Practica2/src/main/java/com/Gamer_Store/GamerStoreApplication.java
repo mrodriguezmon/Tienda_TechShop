be840fea13ff43a1fdf3cbcdbba480cb85314b5b
marco <marco@DESKTOP-H6IVC0U>
2024-06-09 11:45:25 -0600

    Practica_Web_2 semana3 Gamer_Store

---
+++ OneDrive/Documentos/NetBeansProjects/Gamer_Store_Practica2/src/main/java/com/Gamer_Store/GamerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.Gamer_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerStoreApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Gamer_Store_Practica2/src/test/java/com/Gamer_Store/GamerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gamer_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
