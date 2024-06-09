3a69b65e56a55e6f754b029e9d7ced24d24812d1
marco <marco@DESKTOP-H6IVC0U>
2024-06-09 11:29:05 -0600

    Practica_Web_2 Gamer_Store

---
+++ OneDrive/Documentos/NetBeansProjects/Practica_Web_2_GamerStore/src/main/java/com/mycompany/practica_web_2_gamerstore/Practica_Web_2_GamerStore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_web_2_gamerstore;
+
+/**
+ *
+ * @author marco
+ */
+public class Practica_Web_2_GamerStore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
