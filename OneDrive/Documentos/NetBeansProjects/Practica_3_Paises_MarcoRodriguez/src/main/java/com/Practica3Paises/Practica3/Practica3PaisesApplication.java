39c7c81d30d91da22a6bb2b7bacbc28a9faa0b39
marco <marco@DESKTOP-H6IVC0U>
2024-06-30 23:46:47 -0600

    Practica_Web_2 semana3 Gamer_Store

---
+++ OneDrive/Documentos/NetBeansProjects/Practica_3_Paises_MarcoRodriguez/src/main/java/com/Practica3Paises/Practica3/Practica3PaisesApplication.java
@@ -0,0 +1,13 @@
+package com.Practica3Paises.Practica3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3PaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3PaisesApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Practica_3_Paises_MarcoRodriguez/src/test/java/com/Practica3Paises/Practica3/Practica3PaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica3Paises.Practica3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3PaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
