4dc7ed887030ad742d3515d1e7e299fba542598e
marco <marco@DESKTOP-H6IVC0U>
2024-06-30 18:57:16 -0600

    TechShop Tienda

---
+++ OneDrive - Universidad Fidélitas/Escritorio/TiendaTechShop/src/main/java/com/Tienda_TechShop/TiendaTechShopApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_TechShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTechShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTechShopApplication.class, args);
+	}
+
+}
